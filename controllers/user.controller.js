22de67073e2c524c79c1747ffcb196c31274f285
Eric Winebrenner <ejwinebrenner@gmail.com>
2023-12-12 15:50:36 -0500

    completed notes done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,22 +1,40 @@
 const router = require('express').Router();
 const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.JWT;
+
+// const testingBcrypt = password => {
+//     let encrypt = bcrypt.hashSync(password, 10);
+//     console.log("ENCRYPT: ", encrypt)
+// }
 
 //! Signup
-router.post('/signup', async(req,res) => {
+router.post('/signup', async (req, res) => {
+
+    // testingBcrypt('password');
+    // testingBcrypt('password');
+    // testingBcrypt('NEWpassword');
+
     try {
 
         const user = new User({
             firstName: req.body.first,
             lastName: req.body.last,
             email: req.body.email,
-            password: req.body.password
+            // password: req.body.password
+            password: bcrypt.hashSync(req.body.password, 13) // passing in the password provided by the client, salting 13 times.
         });
-        
+
         const newUser = await user.save();
+
+        // const token = jwt.sign({id: newUser._id}, "My Secret Passcode", {expiresIn: "1 day"});
+        const token = jwt.sign({id: newUser._id}, SECRET, {expiresIn: "1 day"});
 
         res.status(200).json({
             user: newUser,
-            message: "Success"
+            message: "Success",
+            token
         })
 
     } catch (err) {
@@ -27,20 +45,36 @@ router.post('/signup', async(req,res) => {
 });
 
 //! Login
-router.post('/login', async (req,res) => {
+router.post('/login', async (req, res) => {
     try {
-        
+
         //1. Capture data provided by user (body).
+        const { email, password } = req.body;
 
         //2. Check database to see if email supplied exists
+        const user = await User.findOne({email: email});
+        //* A MongoDB method that accepts a query as an argument. Returns an instance of a document that matches.
 
-        //3. If email exists, consider if email matches.
+        // console.log(user);
+        if(!user) throw new Error('Email or Password does not match.');
+        
+        //3. If email exists, consider if password matches.
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        //* compare(string, hashed);
+        // console.log(passwordMatch);
+        if(!passwordMatch) throw new Error('Email or Password does not match.');
 
         //4. After verified, provide a jwt token
+        const token = jwt.sign({id: user._id}, SECRET, {expiresIn: 60 * 60 * 12});
 
         //5. response status returned
-        
-        
+        res.status(200).json({
+            message: "Logged in!",
+            user,
+            token
+        })
+
+
     } catch (err) {
         res.status(500).json({
             ERROR: err.message
